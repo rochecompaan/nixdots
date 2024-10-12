38a687ffc71611e995d97ef1dd7193136e947d78
Roché Compaan <roche@upfrontsoftware.co.za>
2024-10-12 06:24:16 +0200

    :wrench: (i18n) switch en_ZA.UTF-8

--- modules/nixos/core/i18n.nix
+++ modules/nixos/core/i18n.nix
@@ -2,15 +2,15 @@
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_ADDRESS = "fr_FR.UTF-8";
-      LC_IDENTIFICATION = "fr_FR.UTF-8";
-      LC_MEASUREMENT = "fr_FR.UTF-8";
-      LC_MONETARY = "fr_FR.UTF-8";
-      LC_NAME = "fr_FR.UTF-8";
-      LC_NUMERIC = "fr_FR.UTF-8";
-      LC_PAPER = "fr_FR.UTF-8";
-      LC_TELEPHONE = "fr_FR.UTF-8";
-      LC_TIME = "fr_FR.UTF-8";
+      LC_ADDRESS = "en_ZA.UTF-8";
+      LC_IDENTIFICATION = "en_ZA.UTF-8";
+      LC_MEASUREMENT = "en_ZA.UTF-8";
+      LC_MONETARY = "en_ZA.UTF-8";
+      LC_NAME = "en_ZA.UTF-8";
+      LC_NUMERIC = "en_ZA.UTF-8";
+      LC_PAPER = "en_ZA.UTF-8";
+      LC_TELEPHONE = "en_ZA.UTF-8";
+      LC_TIME = "en_ZA.UTF-8";
     };
   };
 }

27dd3c954eaa8c5d6eb682ba143c0ad92eae2a48
Roché Compaan <roche@upfrontsoftware.co.za>
2024-10-13 08:40:18 +0200

    :wrench: (hardward) bluetooth, nvidia, smartcards, qmk settings

--- modules/nixos/core/hardware.nix
+++ modules/nixos/core/hardware.nix
@@ -4,9 +4,18 @@
     bluetooth.input.General = {
       ClassicBondedOnly = false;
     };
+    bluetooth.powerOnBoot = true;
     graphics = {
       enable = true;
       enable32Bit = true;
     };
+    gpgSmartcards.enable = true;
+    ledger.enable = true;
+    nvidia.open = false;
+    nvidia.powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
+    keyboard.qmk.enable = true;
   };
 }

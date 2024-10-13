07f5d08a6ae1f8cef77dc2d72c803b389bf72ab8
Roché Compaan <roche@upfrontsoftware.co.za>
2024-10-13 14:54:29 +0200

    :wrench: (kernel) use latest kernel

--- hosts/kiptum/default.nix
+++ hosts/kiptum/default.nix
@@ -1,10 +1,11 @@
-{ inputs, ... }:
+{ pkgs, inputs, ... }:
 {
   imports = [
     inputs.hm.nixosModule
     inputs.nixos-hardware.nixosModules.asus-zephyrus-ga402x-nvidia
     ./hardware-configuration.nix
   ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   networking.hostName = "kiptum";
   networking.hosts = {
     "192.168.1.4" = [ "sidecar" "sidecar.local" "kipchoge" ];

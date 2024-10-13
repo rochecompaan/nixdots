5eb406531a8087b675868c94d5215c739b861381
Roché Compaan <roche@upfrontsoftware.co.za>
2024-10-13 14:05:58 +0200

    :wrench: (programs) add programs from old config

--- modules/nixos/opt/programs/default.nix
+++ modules/nixos/opt/programs/default.nix
@@ -12,6 +12,15 @@
     zsh.enable = true;
     dconf.enable = true;
     wshowkeys.enable = true;
+    openvpn3.enable = true;
+    seahorse.enable = true; # keyring graphical frontend
+    _1password.enable = true;
+    _1password-gui = {
+      enable = true;
+      # Certain features, including CLI integration and system authentication support,
+      # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+      polkitPolicyOwners = [ "roche" ];
+    };
   };
   imports = [ ./steam.nix ];
 }

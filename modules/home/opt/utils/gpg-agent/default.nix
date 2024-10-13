ed4fa70fd85c6c576f916b0a001e38b4a9103464
Roché Compaan <roche@upfrontsoftware.co.za>
2024-10-13 14:07:38 +0200

    :wrench: (gpg-agent) enable ssh agent; set cache ttl

--- modules/home/opt/utils/gpg-agent/default.nix
+++ modules/home/opt/utils/gpg-agent/default.nix
@@ -1,13 +1,14 @@
-{
-  pkgs,
-  lib,
-  config,
-  ...
+{ pkgs
+, lib
+, config
+, ...
 }:
 {
   config = lib.mkIf config.modules.gpg-agent.enable {
     services.gpg-agent = {
       enable = true;
+      enableSshSupport = true;
+      defaultCacheTtl = 3600;
       pinentryPackage = pkgs.pinentry-qt;
     };
   };

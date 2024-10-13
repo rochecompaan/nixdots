b38f800b3475a12d3502162b9945217c30cb2a7e
Roché Compaan <roche@upfrontsoftware.co.za>
2024-10-13 11:05:46 +0200

    :wrench: (fonts) update fonts

--- modules/nixos/opt/fonts/default.nix
+++ modules/nixos/opt/fonts/default.nix
@@ -1,20 +1,23 @@
-{
-  pkgs,
-  lib,
-  config,
-  ...
+{ pkgs
+, lib
+, config
+, ...
 }:
 {
   config = lib.mkIf config.fonts.enable {
     fonts.packages = with pkgs; [
       # icon fonts
+      fira-code-symbols
       material-design-icons
+      noto-fonts-emoji
 
       # normal fonts
       rubik
       lexend
       noto-fonts
       roboto
+      liberation_ttf
+      fira-code
 
       (nerdfonts.override {
         fonts = [

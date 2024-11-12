f2b443d3b41ecc0a8a7d42c372c14e727c339331
Roché Compaan <roche@upfrontsoftware.co.za>
2024-11-12 09:40:24 +0200

    :bug: (zsh) properly escape $ config

--- modules/home/opt/shell/zsh/default.nix
+++ modules/home/opt/shell/zsh/default.nix
@@ -115,9 +115,9 @@
 
         #compdef toggl
         _toggl() {
-          eval $(env COMMANDLINE="$${words[1,$CURRENT]}" _TOGGL_COMPLETE=complete-zsh  toggl)
+          eval \$(env COMMANDLINE="\''${words[1,\$CURRENT]}" _TOGGL_COMPLETE=complete-zsh  toggl)
         }
-        if [[ "$(basename -- $${(%):-%x})" != "_toggl" ]]; then
+        if [[ "\$(basename -- \''${(%):-%x})" != "_toggl" ]]; then
           compdef _toggl toggl
         fi
       '';

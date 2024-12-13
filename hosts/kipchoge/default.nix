95f7e952dd8efc092f8643fed368953d6f7a26b4
Roché Compaan <roche@upfrontsoftware.co.za>
2024-12-13 07:27:06 +0200

    feat: add mosquitto and home assistent to kipchoge

--- hosts/kipchoge/default.nix
+++ hosts/kipchoge/default.nix
@@ -44,4 +44,38 @@
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ];
 
+  services.mosquitto = {
+    enable = true;
+    listeners = [
+      {
+        address = "192.168.1.4";
+        port = 1883;
+        acl = [ "pattern readwrite #" ];
+        omitPasswordAuth = true;
+        settings.allow_anonymous = true;
+      }
+    ];
+  };
+
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers.homeassistant = {
+      volumes = [ "/home/roche/home-assistant:/config" ];
+      environment.TZ = "Africa/Johannesburg";
+      # Warning: if the tag does not change, the image will not be updated
+      image = "ghcr.io/home-assistant/home-assistant:stable";
+      autoStart = true;
+      extraOptions = [
+        "--privileged"
+        "--network=host"
+      ];
+    };
+  };
+
+  services.openvpn.servers = {
+    urbint-vpn = {
+      config = ''config /home/roche/.config/openvpn/urbint.ovpn '';
+      updateResolvConf = true;
+    };
+  };
 }

2beb83063b6b6970f1c4b157f96e7def99c011a7
Roché Compaan <roche@upfrontsoftware.co.za>
2024-12-30 09:13:41 +0200

    feat: create ssh private key from sops secret

--- modules/nixos/core/sops.nix
+++ modules/nixos/core/sops.nix
@@ -12,6 +12,12 @@
         keyFile = "/var/lib/sops-nix/keys.txt";
         sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       };
+
+      secrets = {
+        "private_keys/roche" = {
+          path = "/home/roche/.ssh/id_ed25519";
+        };
+      };
     };
   };
 }

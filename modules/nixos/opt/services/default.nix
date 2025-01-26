531e928003eee99cdea41b0484de98f8f3e7deed
Roché Compaan <roche@upfrontsoftware.co.za>
2025-01-26 16:18:37 +0200

    chore: remove unused services

--- modules/nixos/opt/services/default.nix
+++ modules/nixos/opt/services/default.nix
@@ -1,13 +1,5 @@
 {
   imports = [
-    ./cloudflared-tunnel.nix
     ./duckdns.nix
-    ./glance.nix
-    ./immich.nix
-    ./kanata.nix
-    ./paperless.nix
-    ./radicle.nix
-    ./soft-serve.nix
-    ./vikunja.nix
   ];
 }

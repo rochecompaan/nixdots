a6393e82aed5d06ddada1a7dc2fe8c294cd5e27f
Roché Compaan <roche@sixfeetup.com>
2025-03-01 16:13:41 +0200

    feat: add homelab nodes

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,27 +1,77 @@
 { self, inputs, ... }:
 {
-  flake.nixosConfigurations =
-    let
-      inherit (inputs.nixpkgs.lib) nixosSystem;
-      inherit (import "${self}/modules/nixos") default;
+  flake = {
+    nixosConfigurations =
+      let
+        inherit (inputs.nixpkgs.lib) nixosSystem;
+        inherit (import "${self}/modules/nixos") default;
 
-      specialArgs = {
-        inherit inputs self;
+        specialArgs = {
+          inherit inputs self;
+        };
+
+        mkHost =
+          hostname:
+          nixosSystem {
+            inherit specialArgs;
+            modules = default ++ [
+              inputs.disko.nixosModules.disko
+              ./${hostname}
+            ];
+          };
+      in
+      {
+        kiptum = mkHost "kiptum";
+        kipchoge = mkHost "kipchoge";
+        dauwalter = mkHost "dauwalter";
       };
 
-      mkHost =
-        hostname:
-        nixosSystem {
-          inherit specialArgs;
-          modules = default ++ [
-            inputs.disko.nixosModules.disko
-            ./${hostname}
-          ];
+    # homelab nodes
+    deploy.nodes = [
+      {
+        name = "dauwalter";
+        value = {
+          hostname = "192.168.1.100";
+          profiles.system = {
+            user = "root";
+            sshUser = "nix";
+            path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.dauwalter;
+          };
         };
-    in
-    {
-      kiptum = mkHost "kiptum";
-      kipchoge = mkHost "kipchoge";
-      dauwalter = mkHost "dauwalter";
-    };
+      }
+      {
+        name = "kiptum";
+        value = {
+          hostname = "192.168.1.101";
+          profiles.system = {
+            user = "root";
+            sshUser = "nix";
+            path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.dauwalter;
+          };
+        };
+      }
+      {
+        name = "fordyce";
+        value = {
+          hostname = "192.168.1.102";
+          profiles.system = {
+            user = "root";
+            sshUser = "nix";
+            path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.dauwalter;
+          };
+        };
+      }
+      {
+        name = "walmsley";
+        value = {
+          hostname = "192.168.1.103";
+          profiles.system = {
+            user = "root";
+            sshUser = "nix";
+            path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.dauwalter;
+          };
+        };
+      }
+    ];
+  };
 }
---
+++ hosts/fordyce/default.nix
@@ -0,0 +1,36 @@
+{
+  imports = [
+    ./disko.nix
+    ./hardware-configuration.nix
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking = {
+    hostName = "fordyce";
+    interfaces = {
+      eno1 = {
+        ipv4 = {
+          addresses = [
+            {
+              address = "192.168.1.102";
+              prefixLength = 24;
+            }
+          ];
+        };
+      };
+    };
+    defaultGateway = "192.168.1.1";
+    nameservers = [ "192.168.1.1" ];
+  };
+
+  # Swap configuration
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 8196; # Size in MB (8GB)
+    }
+  ];
+}

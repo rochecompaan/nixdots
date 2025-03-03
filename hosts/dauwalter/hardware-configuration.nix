8c1adec860ef5fc5088abebac93ec0d66ac6f57b
Roché Compaan <roche@sixfeetup.com>
2025-03-03 10:44:26 +0200

    chore: disable dhcp on homelab servers

--- hosts/dauwalter/hardware-configuration.nix
+++ hosts/dauwalter/hardware-configuration.nix
@@ -26,15 +26,7 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  # networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s20f0u2.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp58s0u1u1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp59s0.useDHCP = lib.mkDefault true;
-
+  networking.useDHCP = lib.mkDefault false;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }

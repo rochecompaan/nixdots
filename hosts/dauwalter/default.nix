09bb1b1c79f3911519fecce60310ed64952fa993
Roché Compaan <roche@sixfeetup.com>
2025-03-03 10:54:29 +0200

    refactor: Update network interface configuration and add commented k3s setup

--- hosts/dauwalter/default.nix
+++ hosts/dauwalter/default.nix
@@ -1,3 +1,20 @@
+# { config, lib, ... }:
+# let
+#   kubeVipRbac = builtins.fetchurl {
+#     url = "https://kube-vip.io/manifests/rbac.yaml";
+#     sha256 = ""; # sha256sum of the file can change due to updates to the rbac
+#   };
+#
+#   kubeVipCloudController = builtins.fetchurl {
+#     url = "https://raw.githubusercontent.com/kube-vip/kube-vip-cloud-provider/main/manifest/kube-vip-cloud-controller.yaml";
+#     sha256 = ""; # sha256sum of the file can change due to updates to the cloud-controller
+#   };
+#
+#   kubeVipConfigmap = ./kube-vip-configmap.yaml;
+#
+#   kubeVipManifest = ./kube-vip-manifest.yaml;
+#
+# in
 {
   imports = [
     ./disko.nix
@@ -11,7 +28,7 @@
   networking = {
     hostName = "dauwalter";
     interfaces = {
-      enp0s20f0u2 = {
+      eno1 = {
         ipv4 = {
           addresses = [
             {
@@ -24,6 +41,13 @@
     };
     defaultGateway = "192.168.1.1";
     nameservers = [ "192.168.1.1" ];
+  };
+
+  # https://nixos.org/manual/nixos/stable/#sec-custom-ifnames
+  # alias network interface to ensure kube-vip interface name is consistent
+  systemd.network.links."10-rename-interface" = {
+    matchConfig.PermanentMACAddress = "f0:a7:31:b0:b5:4c";
+    linkConfig.Name = "eno1";
   };
 
   # Swap configuration
@@ -47,4 +71,42 @@
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
+
+  # services.k3s = {
+  #   enable = true;
+  #   clusterInit = true;
+  #   role = "server";
+  #   tokenFile = config.sops.secrets."cluster-token".path;
+  #
+  #   extraFlags = lib.concatStringsSep " " [
+  #     "--node-ip=192.168.1.100"
+  #     "--disable=traefik"
+  #     "--tls-san=192.168.1.100"
+  #     "--tls-san=192.168.1.200" # kube-vip VIP
+  #     "--write-kubeconfig-mode=0644"
+  #   ];
+  #
+  #   manifests = {
+  #     argocd.content = {
+  #       apiVersion = "helm.cattle.io/v1";
+  #       kind = "HelmChart";
+  #       metadata = {
+  #         name = "argocd";
+  #         namespace = "kube-system";
+  #       };
+  #       spec = {
+  #         targetNamespace = "argocd";
+  #         createNamespace = true;
+  #         repo = "https://argoproj.github.io/argo-helm";
+  #         chart = "argo-cd";
+  #         version = "7.7.16";
+  #         valuesContent = ''
+  #           configs:
+  #             params:
+  #               server.insecure: true
+  #         '';
+  #       };
+  #     };
+  #   };
+  # };
 }

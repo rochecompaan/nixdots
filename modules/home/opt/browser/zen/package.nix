00fcf043fdfb4fc8c2f80f26f169dac74548c3bd
Roché Compaan <roche@sixfeetup.com>
2025-04-19 13:26:29 +0200

    chore: update zen-browser app image sha

--- modules/home/opt/browser/zen/package.nix
+++ modules/home/opt/browser/zen/package.nix
@@ -11,7 +11,7 @@ let
 
   src = fetchurl {
     url = "https://github.com/zen-browser/desktop/releases/latest/download/zen-x86_64.AppImage";
-    sha256 = "sha256-1NoDIdzeADVLOhtesR0QZefhAdRBSNWg4++MjqdkfIs=";
+    sha256 = "sha256-zByr5UOXicM9luBJ1lQ/INjRwOF07LPrEznJ/XuuNw8=";
   };
 
   appimageContents = appimageTools.extract {

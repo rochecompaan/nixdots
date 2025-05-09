c98c5892bc632dfb0ec33ff1f41ffa83dcd237f8
Roché Compaan <roche@sixfeetup.com>
2025-05-09 20:00:10 +0200

    feat: overlay to install unstable zoom and zellij

--- modules/home/core/overlays.nix
+++ modules/home/core/overlays.nix
@@ -18,31 +18,28 @@
               '';
           });
     })
-    # Zellij 0.41.2 overlay
     (_: prev: {
-      zellij = prev.zellij.overrideAttrs (_: rec {
-        version = "0.41.2";
-        name = "zellij";
-
-        src = prev.fetchFromGitHub {
-          owner = "zellij-org";
-          repo = "zellij";
-          rev = "v0.41.2";
-          hash = "sha256-xdWfaXWmqFJuquE7n3moUjGuFqKB90OE6lqPuC3onOg=";
-        };
-
-        postPatch = ''
-          substituteInPlace Cargo.toml \
-            --replace-fail ', "vendored_curl"' ""
-        '';
-        cargoDeps = prev.rustPlatform.fetchCargoTarball {
-          inherit src;
-          name = "${name}-${version}";
-          hash = "sha256-38hTOsa1a5vpR1i8GK1aq1b8qaJoCE74ewbUOnun+Qs=";
-        };
-      });
+      zellij =
+        let
+          unstable = import inputs.nixpkgs-unstable {
+            inherit (prev) system;
+          };
+        in
+        unstable.zellij;
     })
     (_: prev: { zjstatus = inputs.zjstatus.packages.${prev.system}.default; })
+    (_: prev: {
+      zoom-us =
+        let
+          unstable = import inputs.nixpkgs-unstable {
+            inherit (prev) system;
+            config = {
+              allowUnfree = true;
+            };
+          };
+        in
+        unstable.zoom-us;
+    })
     (_: prev: {
       _1password-gui =
         let

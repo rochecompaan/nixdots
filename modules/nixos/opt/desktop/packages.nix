0e9a85c79a72ec52b58eeb425884dcd068826ecc
Roché Compaan <roche@sixfeetup.com>
2025-06-08 12:20:34 +0200

    chore: use cura-appimage package

--- modules/nixos/opt/desktop/packages.nix
+++ modules/nixos/opt/desktop/packages.nix
@@ -12,6 +12,7 @@ in
     environment.systemPackages = with pkgs; [
       # Media & Creative
       blender
+      cura-appimage
       ffmpeg_7-full
       mpv
       obs-studio
@@ -43,31 +44,6 @@ in
       yubikey-manager
       yubikey-personalization
 
-      # 3D Printing
-      (
-        let
-          cura5 = appimageTools.wrapType2 rec {
-            name = "cura5";
-            version = "5.4.0";
-            src = fetchurl {
-              url = "https://github.com/Ultimaker/Cura/releases/download/${version}/UltiMaker-Cura-${version}-linux-modern.AppImage";
-              hash = "sha256-QVv7Wkfo082PH6n6rpsB79st2xK2+Np9ivBg/PYZd74=";
-            };
-            extraPkgs = pkgs: with pkgs; [ ];
-          };
-        in
-        writeScriptBin "cura" ''
-          #! ${pkgs.bash}/bin/bash
-          args=()
-          for a in "$@"; do
-            if [ -e "$a" ]; then
-              a="$(realpath "$a")"
-            fi
-            args+=("$a")
-          done
-          QT_QPA_PLATFORM=xcb exec "${cura5}/bin/cura5" "''${args[@]}"
-        ''
-      )
     ];
   };
 }

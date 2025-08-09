274447657b0405bc469a24f9055c7b087c9fae1c
Roché Compaan <roche@sixfeetup.com>
2025-08-09 09:01:33 +0200

    feat(nixos): set timezone to Africa/Johannesburg

--- modules/nixos/core/time.nix
+++ modules/nixos/core/time.nix
@@ -1,15 +1,16 @@
 {
   time = {
     hardwareClockInLocalTime = true;
+    timeZone = "Africa/Johannesburg";
   };
-  services.tzupdate.enable = true;
+  services.tzupdate.enable = false;
 
-  systemd.timers.tzupdate = {
-    description = "Update timezone automatically";
-    timerConfig = {
-      OnCalendar = "hourly";
-      Persistent = true;
-    };
-    wantedBy = [ "timers.target" ];
-  };
+  # systemd.timers.tzupdate = {
+  #   description = "Update timezone automatically";
+  #   timerConfig = {
+  #     OnCalendar = "hourly";
+  #     Persistent = true;
+  #   };
+  #   wantedBy = [ "timers.target" ];
+  # };
 }

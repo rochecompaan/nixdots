167cb9b8c4e855960b4acf8558c72f837ff56ea4
Roché Compaan <roche@sixfeetup.com>
2025-11-12 15:32:51 +0200

    chore: workspace config on kiptum

--- home/roche/kiptum.nix
+++ home/roche/kiptum.nix
@@ -58,6 +58,20 @@
       "HDMI-A-1, 1920x1080@74.97, 0x0, auto"
       "eDP-1, 1920x1080@165, 1920x0, auto"
     ];
+
+    # workspace rules
+    workspace = [
+      "1,monitor:HDMI-A-1"
+      "2,monitor:HDMI-A-1"
+      "3,monitor:HDMI-A-1"
+      "4,monitor:HDMI-A-1"
+      "5,monitor:HDMI-A-1"
+      "6,monitor:HDMI-A-1"
+      "7,monitor:HDMI-A-1"
+      "8,monitor:eDP-1"
+      "9,monitor:eDP-1"
+      "10,monitor:eDP-1"
+    ];
   };
 
   home = {

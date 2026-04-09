3f61e9573805a5d9d151cb8e39b8aaf916a5bb96
Roché Compaan <roche@sixfeetup.com>
2026-04-09 13:23:23 +0200

    feat(pi): add themes

--- modules/home/desktop/utils/pi/default.nix
+++ modules/home/desktop/utils/pi/default.nix
@@ -1,6 +1,21 @@
 {
   home.file = {
+    ".pi/agent/settings.json".source = ./settings.json;
+
     ".pi/agent/extensions/filter-output.ts".source = ./filter-output.ts;
     ".pi/agent/extensions/security.ts".source = ./security.ts;
+    ".pi/agent/extensions/theme-cycler.ts".source = ./theme-cycler.ts;
+
+    ".pi/agent/themes/catppuccin-mocha.json".source = ./themes/catppuccin-mocha.json;
+    ".pi/agent/themes/cyberpunk.json".source = ./themes/cyberpunk.json;
+    ".pi/agent/themes/dracula.json".source = ./themes/dracula.json;
+    ".pi/agent/themes/everforest.json".source = ./themes/everforest.json;
+    ".pi/agent/themes/gruvbox.json".source = ./themes/gruvbox.json;
+    ".pi/agent/themes/midnight-ocean.json".source = ./themes/midnight-ocean.json;
+    ".pi/agent/themes/nord.json".source = ./themes/nord.json;
+    ".pi/agent/themes/ocean-breeze.json".source = ./themes/ocean-breeze.json;
+    ".pi/agent/themes/rose-pine.json".source = ./themes/rose-pine.json;
+    ".pi/agent/themes/synthwave.json".source = ./themes/synthwave.json;
+    ".pi/agent/themes/tokyo-night.json".source = ./themes/tokyo-night.json;
   };
 }
